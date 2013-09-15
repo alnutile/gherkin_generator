fdc1a137f52a6205e63311d2abc2c6b019b1390a
alnutile <alfrednutile@gmail.com>
2013-09-15 17:33:32 -0400

    File save in place

--- js/gherkin_run.js
+++ js/gherkin_run.js
@@ -3,7 +3,6 @@
     Drupal.behaviors.gherkin_generator_run = {
         attach: function (context) {
             var renderMessage = function(message) {
-
                 var messages = "<div class='alert alert-info'>";                //@todo pull out error = FALSE/TRUE
                 messages += message;                                            //@todo pull out error type eg error, info, success etc
                 messages += "</div>";
@@ -17,8 +16,12 @@
                 for(var i = 0; i < items; i++) {
                     scenario_array[i] =$(scenario[i]).text();
                 }
+
+                var filename = $('input[name=filename]').val();
+                console.log("Filename" + filename);
                 var parameters = {
-                    "scenario[]": scenario_array
+                    "scenario[]": scenario_array,
+                    "filename": filename
                 };
                 $.post('/admin/gherkin_generator/run', parameters, function(data){
                         console.log(data);

de5088869fc459a1cc7b38acb11a2ce9060efe44
alnutile <alfrednutile@gmail.com>
2013-09-17 14:31:03 -0400

    Add dynamic path

--- js/gherkin_save.js
+++ js/gherkin_save.js
@@ -36,12 +36,16 @@
                 for(var i = 0; i < items; i++) {
                     scenario_array[i] = $(scenario[i]).text();
                 }
-
+                if($('#edit-save-to').length == 1) {
+                    var path = $('#edit-save-to option:selected').val();
+                } else {
+                    var path = Drupal.settings.gherkin_generator.gherkinGeneratorDefaultPath;
+                }
                 var filename = $('input[name=filename]').val();
-                console.log("File name" + filename);
                 var parameters = {
                     "scenario[]": scenario_array,
-                    "filename": filename
+                    "filename": filename,
+                    "path": path
                 };
 
                 $.post('/admin/gherkin_generator/save', parameters, function(data){
